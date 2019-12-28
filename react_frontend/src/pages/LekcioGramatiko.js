c2622a89689fcb8dce5594eb79e2b9089eacdc68
Paul Würtz <paulwuertz@posteo.de>
2019-12-28 17:43:57 +0100

    funkciigu gramatika pagxo

---
+++ react_frontend/src/pages/LekcioGramatiko.js
@@ -0,0 +1,31 @@
+import React, { Component }  from 'react';
+import Button from 'react-bootstrap/Button';
+import { LinkContainer } from 'react-router-bootstrap'
+import Tabs from './Tabs';
+const ReactMarkdown = require('react-markdown')
+
+class LekcioGramatiko extends Component {
+
+  constructor(props){
+    super(props);
+    this.enhavo = props.state
+    this.lekcio = props.lekcio
+  }
+
+  render = () => <div dir={ this.enhavo.tekstodirekto }>
+      <Tabs lekcio = "01" state = {this.enhavo} />
+      <div className="mt-3">
+        <ReactMarkdown source={ this.enhavo["lecionoj"][parseInt(this.lekcio)]["gramatiko"]["teksto"] } />
+
+        <LinkContainer to="/01">
+        <Button>{ this.enhavo.fasado['Ek'] }</Button>
+        </LinkContainer>
+
+        <LinkContainer to="/01">
+        <Button>{ this.enhavo.fasado['Ek'] }</Button>
+        </LinkContainer>
+      </div>
+    </div>
+}
+
+export default LekcioGramatiko;
