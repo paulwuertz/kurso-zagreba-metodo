8ddeabfe660762373bf1782abe7578d057ba9d5f
Paul Würtz <paulwuertz@posteo.de>
2019-12-31 00:18:07 +0100

    aldonu auxtoroj-pagxon+footer link

---
+++ react_frontend/src/pages/Auxtoroj.js
@@ -0,0 +1,54 @@
+import React, { Component }  from 'react';
+import Tabs from './Tabs';
+
+class Auxtoroj extends Component {
+
+  constructor(props){
+    super(props);
+    this.lingvoj = props.lingvoj
+    this.lingvo = props.lingvo
+  }
+
+  auxtoroj = () =>
+    Object.entries(this.lingvoj[this.lingvo]['aŭtoroj']).map(auxtoro =>
+        this.auxtoro(auxtoro[1])
+    )
+
+  auxtoro(auxtoro){
+      if (auxtoro.github)
+        return <li><a href="https://github.com/{auxtoro.github}">{auxtoro.nomo}</a></li>
+      else
+        return <li>{auxtoro.nomo}</li>
+  }
+
+  render = () =>
+    <div>
+        Aŭtoroj | this.enhavo.fasado['Esperanto en 12 tagoj']
+        <h2>Aŭtoroj</h2>
+        <h3>Enhavo</h3>
+        <p>La aŭtoroj de la <em><a href="https://eo.wikipedia.org/wiki/Zagreba_metodo">Zagreba metodo</a></em> estas:</p>
+        <ul>
+          <li><a href="https://eo.wikipedia.org/wiki/Zlatko_Ti%C5%A1ljar">Zlatko Tišljar</a></li>
+          <li><a href="https://eo.wikipedia.org/wiki/Spomenka_%C5%A0timec">Spomenka Štimec</a></li>
+          <li>Ivica Špoljarec</li>
+          <li><a href="https://eo.wikipedia.org/wiki/Roger_Imbert">Roger Imbert</a></li>
+        </ul>
+        <h3>Retejo</h3>
+        <p>La retejon programis:</p>
+        <ul>
+          <li><a href="https://github.com/georgjaehnig/">Georg Jähnig</a></li>
+        </ul>
+        <h3>Voĉlego</h3>
+        <p>La tekstojn voĉlegis:</p>
+        <ul>
+          <li><a href="https://github.com/EmilioCid/">Emilio Cid</a></li>
+        </ul>
+        <h3>Ciferecigo de la {this.lingvoj[this.lingvo].nomo.esperante}</h3>
+        <p>La ciferecigon de la {this.lingvoj[this.lingvo].nomo.esperante} versio faris:</p>
+        <ul>
+            {this.auxtoroj()}
+        </ul>
+    </div>
+}
+
+export default Auxtoroj;
--- react_frontend/src/pages/Menuo.js
+++ react_frontend/src/pages/Menuo.js
@@ -2,7 +2,7 @@ import React, { Component }  from 'react';
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
 
-class Meno extends Component {
+export class Menuo extends Component {
 
   constructor(props){
     super(props);
@@ -17,13 +17,7 @@ class Meno extends Component {
 
   render = () => <Navbar expand="md" bg="dark">
   <Navbar.Brand href="/">
-      <img
-        src="assets/img/stelo.png"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt="React Bootstrap logo"
-      />
+    <img src="assets/img/stelo.png" width="30" height="30" className="d-inline-block align-top" alt="React Bootstrap logo"/>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
@@ -61,4 +55,20 @@ class Meno extends Component {
   </Navbar>
 }
 
-export default Meno;
+export class Footer extends Component {
+  constructor(props) {
+      super(props);
+  }
+  render = () => <footer className="container">
+    <hr className="featurette-divider" style={{marginBottom: 30 + "px"}}/>
+    <div className="float-left" style={{marginBottom: 30 + "px"}}>
+      <a href="https://github.com/Esperanto/kurso-zagreba-metodo#permesiloj">
+          <img id="cc" src="assets/img/cc.png" alt="Creative commons"/>
+      </a>
+      Surbaze de la <a href="https://eo.wikipedia.org/wiki/Zagreba_metodo">Zagreba metodo</a>,
+      <LinkContainer to="/auxtoroj"><a>Aŭtoroj</a></LinkContainer>, 
+      <a href="https://github.com/Esperanto/kurso-zagreba-metodo/tree/master/enhavo/tradukenda">Aldonu lingvon</a>,
+      <a href="https://jaehnig.org/#contact">Kontakto</a>
+    </div>
+  </footer>
+}
